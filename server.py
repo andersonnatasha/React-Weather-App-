81a93ffe39bf4716ea98361b9bba86fddb438d74
Natasha Anderson <andersonnatashaa@gmail.com>
2021-01-22 19:19:50 +0000

    server running

--- server.py
+++ server.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.route("/")
 
-
+@app.route('/')
 def show_homepage():
-    """Show the homepage."""
+    """Show the application's homepage."""
 
-    return render_template("homepage.html")
+    return render_template('homepage.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
